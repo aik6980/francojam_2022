f6f5afde11b2f4afc36e1d3cff2f25b2f697ab71
Neil Doyle <neildoyle.92@gmail.com>
2022-11-26 04:20:10 +0000

    Made the game playable

---
+++ master/Assets/Scripts/woof.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class woof : MonoBehaviour
+{
+    [SerializeField] GameObject goodBoy, dogButton;
+
+    private void OnEnable()
+    {
+        AudioManager.Instance.PlayDogBarkUI(0);
+
+        goodBoy.SetActive(true);
+        dogButton.SetActive(false);
+
+        Invoke("DogButton", 0.3f);
+
+        this.gameObject.SetActive(false);
+    }
+
+    void DogButton()
+    {
+        goodBoy.SetActive(false);
+        dogButton.SetActive(true);
+    }
+}
